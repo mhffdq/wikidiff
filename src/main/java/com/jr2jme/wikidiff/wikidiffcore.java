ae9e4202f3ec40eaf254d7e9d0b253f1bcff83cd
Hirotaka <mhffdq@gmail.com>
2014-04-07 17:50:55 +0900

    1000version 17秒

--- src/main/java/com/jr2jme/wikidiff/wikidiffcore.java
+++ src/main/java/com/jr2jme/wikidiff/wikidiffcore.java
@@ -9,6 +9,7 @@ import net.java.sen.StringTagger;
 import net.java.sen.dictionary.Token;
 import org.mongojack.DBCursor;
 import org.mongojack.DBQuery;
+import org.mongojack.DBSort;
 import org.mongojack.JacksonDBCollection;
 
 import java.io.IOException;
@@ -38,66 +39,70 @@ public class wikidiffcore {
         JacksonDBCollection<Wikitext,String> coll = JacksonDBCollection.wrap(dbCollection, Wikitext.class, String.class);
         DBCollection dbCollection2=db.getCollection("edit_test2");
         JacksonDBCollection<WikiEdit,String> coll2 = JacksonDBCollection.wrap(dbCollection2, WikiEdit.class,String.class);
-        DBCursor<Wikitext> cursor = coll.find(DBQuery.is("title", "Bon Appetit!")).limit(100);
+
         List<String> prev_text=new ArrayList();
-
-
         List<Term_Editor> predata=new ArrayList<Term_Editor>();
-        ExecutorService exec = Executors.newFixedThreadPool(3);
+        ExecutorService exec = Executors.newFixedThreadPool(10);
         Levenshtein3 d = new Levenshtein3();
         int version=0;
-        long start=System.currentTimeMillis();
-        for(final Wikitext wikitext:cursor){
 
-            StringTagger tagger = SenFactory.getStringTagger(null);
-            List<Token> tokens = new ArrayList<Token>();
-            try {
-                tagger.analyze(wikitext.getText(), tokens);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            List<String> current_text=new ArrayList<String>();
-            for(Token token:tokens){
-                //System.out.println(token.getSurface());
-                current_text.add(token.getSurface());
-            }
+        int offset=0;
+        DBCursor<Wikitext> cursor = coll.find(DBQuery.is("title", "亀梨和也").greaterThan("version",offset)).sort(DBSort.asc("version"));
 
+        long start = System.currentTimeMillis();
+        while(cursor.hasNext()) {
+            //List<WikiEdit> wikieditlist = new ArrayList<WikiEdit>(50);
+            for (Wikitext wikitext : cursor) {
 
-            List<String[]> diff = d.diff(prev_text, current_text);
-
-            List<Term_Editor> data=new ArrayList<Term_Editor>();
-            int i = 0;
-            Map<String,List<String>> deleteterms=new HashMap<String, List<String>>();
-                    new ArrayList<String>();
-            for(String[] st:diff){
-                if(st[1].equals("i")){
-                    Term_Editor tes =new Term_Editor(st[0],wikitext.getName());
-                    data.add(tes);
+                //System.out.println(version);
+                StringTagger tagger = SenFactory.getStringTagger(null);
+                List<Token> tokens = new ArrayList<Token>();
+                try {
+                    tagger.analyze(wikitext.getText(), tokens);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                else if(st[1].equals("d")){
-                    if(deleteterms.containsKey(predata.get(i))) {
-                        deleteterms.get(predata.get(i).getName()).add(predata.get(i).getTerm());
-                    }else{
-                        List<String> terms=new ArrayList<String>();
-                        terms.add(predata.get(i).getTerm());
-                        deleteterms.put(predata.get(i).getName(),terms);
+                List<String> current_text = new ArrayList<String>();
+                for (Token token : tokens) {
+                    //System.out.println(token.getSurface());
+                    current_text.add(token.getSurface());
+                }
+
+
+                List<String[]> diff = d.diff(prev_text, current_text);
+                List<Term_Editor> data = new ArrayList<Term_Editor>();
+                int i = 0;
+                Map<String, List<String>> deleteterms = new HashMap<String, List<String>>();
+                new ArrayList<String>();
+                for (String[] st : diff) {
+                    if (st[1].equals("i")) {
+                        Term_Editor tes = new Term_Editor(st[0], wikitext.getName());
+                        data.add(tes);
+                    } else if (st[1].equals("d")) {
+                        if (deleteterms.containsKey(predata.get(i))) {
+                            deleteterms.get(predata.get(i).getName()).add(predata.get(i).getTerm());
+                        } else {
+                            List<String> terms = new ArrayList<String>();
+                            terms.add(predata.get(i).getTerm());
+                            deleteterms.put(predata.get(i).getName(), terms);
+                        }
+                        i++;
+                    } else if (st[1].equals("r")) {
+                        Term_Editor tes = new Term_Editor(st[0], predata.get(i).getName());
+                        data.add(tes);
+                        i++;
                     }
-                    i++;
+
                 }
-                else if(st[1].equals("r")){
-                    Term_Editor tes =new Term_Editor(st[0],predata.get(i).getName());
-                    data.add(tes);
-                    i++;
-                }
+                //wikieditlist.add(new WikiEdit(data,wikitext.getTitle(),version));
+                //coll2.insert(new WikiEdit(data,wikitext.getTitle(),version));
+                exec.submit(new Task(coll2, data, wikitext.getTitle(), version));
+                predata = data;
+                prev_text = current_text;
+                version++;
             }
-
-
-            coll2.insert(new WikiEdit(data,wikitext.getTitle(),version));
-
-            //exec.submit(new Task(coll2,data,wikitext.getTitle(),version));
-            predata=data;
-            prev_text=current_text;
-            version++;
+            offset+=100;
+            cursor = coll.find(DBQuery.is("title", "亀梨和也").greaterThan("version",offset)).sort(DBSort.asc("version"));
         }
         exec.shutdown();
         try {
@@ -105,6 +110,8 @@ public class wikidiffcore {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        //coll2.insert(wikieditlist);
         System.out.println(System.currentTimeMillis() - start);
         cursor.close();
         mongo.close();
@@ -126,8 +133,9 @@ class Task implements Runnable {
     }
     @Override
     public void run(){
+        //long start=System.currentTimeMillis();
         coll.insert(new WikiEdit(data, name, version));
-        System.out.println(version);
+        //System.out.println(System.currentTimeMillis() - start);
     }
     public void stopThread(){
     }
